ec16611f619a0d8deebbc1c9bae271a0e09c70b4
Emirhan Latif <emirhanlatif@outlook.com>
2021-01-23 18:20:36 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<P,S>
+    {
+        P[] plaka;
+        S[] sehir;
+        public MyDictionary()
+        {
+            plaka = new P[0];
+            sehir = new S[0];
+        }
+        P[] tempplaka;
+        S[] tempsehir;
+        public void Add(P plakas,S sehirs)
+        {
+            tempplaka = plaka;
+            tempsehir = sehir;
+            plaka = new P[plaka.Length + 1];
+            sehir = new S[sehir.Length + 1];
+            for (int i = 0; i < tempplaka.Length; i++)
+            {
+                plaka[i] = tempplaka[i];
+            }
+            for (int i = 0; i < tempsehir.Length; i++)
+            {
+                sehir[i] = tempsehir[i];
+            }
+
+            plaka[plaka.Length - 1] = plakas;
+            sehir[sehir.Length - 1] = sehirs;
+        }
+        public void Yazdir()
+        {
+            for (int i = 0; i < plaka.Length; i++)
+            {
+                Console.WriteLine("Plaka: " + plaka[i] +"---" +" Sehir: " + sehir[i]);
+            }
+        }
+        public void Say()
+        {
+            Console.WriteLine(plaka.Length);
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> X = new MyDictionary<int, string>();
+            X.Add(1, "Adana");
+            X.Add(2, "Adıyaman");
+            X.Add(3, "Afyon");
+            X.Add(34, "İstanbul");
+            X.Add(53, "Rize");
+            X.Yazdir();
+            X.Say();             
+        }
+    }
+}
